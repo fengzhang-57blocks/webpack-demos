28076dabff1eef2a3fb5c5b6773e57a739defda9
feng.zhang-57blocks <feng.zhang@vungle.com>
2022-08-12 15:19:39 +0800

    rename basic-demo to basic

--- basic-demo/src/index.js
+++
@@ -1,5 +0,0 @@
-import sum from './helper/sum';
-import '../../basic-demo/src/styles/css/style.css';
-import '../../basic-demo/src/styles/sass/style.scss';
-
-console.log(sum(1, 2, 3));
---
+++ basic/src/index.js
@@ -0,0 +1,5 @@
+import sum from './helper/sum';
+import '../src/styles/css/style.css';
+import '../src/styles/sass/style.scss';
+
+console.log(sum(1, 2, 3));
